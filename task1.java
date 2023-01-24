d2742d5a0e61fc4e6c8b0fe25993f69d3df90493
Alice2506 <ralko84@mail.ru>
2023-01-24 22:56:38 +0300

    made formating

--- task1.java
+++ task1.java
@@ -3,25 +3,25 @@
 Вычислить сумму чисел от 1 до n, вычислить n! произведение чисел от 1 до n
 */
 
-public class task1{
-	static int calculateFactorial(int n){
+public class task1 {
+	static int calculateFactorial(int n) {
 		int result = 1;
-		for (int i = 1; i <=n; i ++){
-			result = result*i;
+		for (int i = 1; i <= n; i++) {
+			result = result * i;
 		}
 		return result;
 	}
-    
-    static int calculateSum(int n){
-        int result = 0;
-        for (int i = 1; i <= n; ++i){
-            result = result + i;
-        }
-        return result;
-    }
- 
-	public static void main(String[] args){
+
+	static int calculateSum(int n) {
+		int result = 0;
+		for (int i = 1; i <= n; ++i) {
+			result = result + i;
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
 		System.out.println(calculateFactorial(3));
-        System.out.println(calculateSum(4)); 
+		System.out.println(calculateSum(4));
 	}
 }
